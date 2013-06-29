db798b9660a5ee2574dc6ea9112f0de4ba7a1796
ziuloliveira@gmail.com <ziuloliveira@gmail.com@ceb656d0-5844-6d09-e0a4-2b68a341c80b>
2013-06-29 20:11:32 +0000

    #Criação do modulo de saída.    - necessario entrar apenas uma classe que contenha o atributo path com o endereço das tabelas # Revisão da ui.py para ser apenas interface TODO: Criação de abas

--- trunk/Ramses/ui.py
+++ trunk/Ramses/ui.py
@@ -3,7 +3,7 @@
 
 '''locais'''
 from pontos import *
-import janela
+import output
 
 '''externos'''
 try:
@@ -31,6 +31,7 @@ class Form(wx.Frame):
 	def __init__(self,path):
 		wx.Frame.__init__(self, None, wx.ID_ANY,self.name,size=(500,500))
 		
+		self.path=path
 		self.Painel = wx.Panel(self)
 		self.sair = wx.Button(self.Painel, 1, "Sair",(400,30))
 		self.dados=water(path)
@@ -47,8 +48,8 @@ class Form(wx.Frame):
 		self.second.SetStringSelection(options[0])
 		self.second_value = wx.TextCtrl(self.Painel, -1, "25", size=(50, -1),pos=(270,60),style=wx.TE_PROCESS_ENTER)
 		
-		self.third =  wx.StaticText(self.Painel, label=self.dados.dados['more'][self.first.GetSelection()], pos=(50, 90))
-		self.third.Show(False)
+		self.third =  wx.StaticText(self.Painel, label='Pressão [MPa]', pos=(50, 90))
+		self.third.Hide()
 		self.third_value = wx.ComboBox(self.Painel, pos=(270, 90), size=(90, -1), choices=[], style=wx.CB_SORT)
 		self.third_value.Hide()
 		
@@ -68,110 +69,10 @@ class Form(wx.Frame):
 	def show(self,event):
 		self.third.Hide()
 		self.third_value.Hide()
-
-		try:
-			self.dados=water(path)
-			escolha1 = self.dados.dados['names'][self.first.GetSelection()]
-			escolha2 = self.dados.dados['names'][self.second.GetSelection()+2]
-			value1 = float(self.first_value.GetValue())
-			value2 = float(self.second_value.GetValue())
-			inter_min = sp.interp1d(self.dados.dados[escolha1], self.dados.dados[escolha2+"_min"],kind='linear')
-			inter_max = sp.interp1d(self.dados.dados[escolha1], self.dados.dados[escolha2+"_max"],kind='linear')
-		except Exception, ex:
-			self.results= "Dados vazios ou não validos\n\n" + str(ex)
-			self.text.SetValue(self.results)
-
-		try:
-			valores=''
-			if (value2>=inter_min(value1)):
-				if (value2<=inter_max(value1)):
-					valores= "Esta no estado de água saturada\n".center(80) + "\n"
-					for i,u in zip(self.dados.dados['index'],self.dados.dados['unit_index']):
-						inter = sp.interp1d(self.dados.dados[escolha1], self.dados.dados[i],kind='linear')
-						valores += ( i.rjust(13) +"\t\t=\t" + (str(inter(value1))+" "+ u).ljust(11)  + "\n")
-					titulo =((inter(value1) - inter_min(value1)))/ (inter_min(value1)+inter_max(value1))
-					valores += "        Titulo\t\t=\t"  + str(titulo) + "\n"
-					valores += "----------------------------------------------------------------------------------------\n"
-					valores += "Valores precisos:\n".center(90) + "\n"
-					for i,u in zip(self.dados.dados['names'][2:],self.dados.dados['unit'][2:]):
-						if((i != 'Pressure') and (i != 'Temperature') ):
-							precise = self.dados.dados[i+'_min'] + titulo * (self.dados.dados[i+'_min'] + self.dados.dados[i+'_max'])/2
-							inter = sp.interp1d(self.dados.dados[escolha1], precise,kind='linear')
-							valores += ( i.rjust(13) +"\t\t=\t" + (str(inter(value1))+" "+ u).ljust(11)  + "\n")
-					
-					
-				else:
-					valores= "Esta no estado vapor superaquecido\n".center(80) + "\n"	# (acima do intervalo para saturada)
-					lista=['10', '50', '100' , '200',  '300',  '400',  '500' , '600',  '800','1000', '2000','3000','4000','5000','10000','20000','30000','40000','50000','60000']
-					print "Esta no estado vapor superaquecido"
-					self.third =  wx.StaticText(self.Painel, label='Pressão [kPa]', pos=(50, 90))
-					value1 = self.third_value.GetSelection()
-					self.third_value.Clear()
-					for i in lista:
-						self.third_value.Append(i)
-					self.third_value.SetStringSelection(lista[value1])
-					self.third.Show(True)
-					self.third_value.Show(True)
-					value1 = self.third_value.GetSelection()
-					'''
-					if (lista.index(value1)):
-						j=janela.choose()
-						j.buildme("Não é saturada","Entre novamente como o valor para a Pressão [kPa]",lista)
-						j.Show(False)
-						value1 = j.selection
-						self.first_value.SetValue( lista[value1])
-					'''
-					self.dados=waterNext('./a6/' + lista[value1] + '.csv')
-					for i in self.dados.dados['Temperature']:
-						self.dados.dados['Pressure'] = numpy.append(self.dados.dados['Pressure'],float(lista[value1]))
-					for i,u in zip(self.dados.dados['index'],self.dados.dados['unit_index']):
-						inter = sp.interp1d(self.dados.dados[escolha2], self.dados.dados[i],kind='linear')
-						valores += ( i.rjust(13) +"\t\t=\t" + (str(inter(value2))+" "+ u).ljust(11)  + "\n")
-
-			else:
-				valores= "Esta no estado liquido comprimido\n".center(80) + "\n"
-				lista=['5','10','15','20','30','50']
-				print "Esta no estado liquido comprimido"
-				self.third =  wx.StaticText(self.Painel, label='Pressão [MPa]', pos=(50, 90))
-				value1 = self.third_value.GetSelection()
-				self.third_value.Clear()
-				for i in lista:
-					self.third_value.Append(i)
-				self.third_value.SetStringSelection(lista[value1])
-				self.third.Show(True)
-				self.third_value.Show(True)
-				value1 = self.third_value.GetSelection()
-				'''
-				if (lista.index(value1)):
-					j=janela.choose()
-					j.buildme("Não é saturada","Entre novamente como o valor para a Pressão [kPa]",lista)
-					j.Show(False)
-					value1 = j.selection
-					self.first_value.SetValue( lista[value1])
-				'''
-				self.dados=waterNext('./a7/' + lista[value1] + '.csv')
-				for i in self.dados.dados['Temperature']:
-					self.dados.dados['Pressure'] = numpy.append(self.dados.dados['Pressure'],float(lista[value1]))
-				for i,u in zip(self.dados.dados['index'],self.dados.dados['unit_index']):
-					inter = sp.interp1d(self.dados.dados[escolha2], self.dados.dados[i],kind='linear')
-					valores += ( i.rjust(13) +"\t\t=\t" + (str(inter(value2))+" "+ u).ljust(11)  + "\n")
-			
-					
-		except ValueError, ex:
-			valores+= "\nFaixa de valores fora do intervalo de amostra\n" + str(ex)
-			valores+= "\n\nProvavelmente fora do intervalo de interpolação\n\n\n" 
-			
-			valores += "%s: %.2e - %.2e\n" % (self.dados.dados['more'][self.first.GetSelection()],min(self.dados.dados[escolha1]) , max(self.dados.dados[escolha1]))
-			
-			try:
-				print "%s: %.2e - %.2e\n" % (self.dados.dados['more'][self.second.GetSelection()],min(self.dados.dados[escolha2]) , max(self.dados.dados[escolha2]) )
-			except Exception, ex:
-				valores += "%s minima: %.2e - %.2e\n" % (self.dados.dados['more'][self.second.GetSelection()+2],min(self.dados.dados[escolha2+'_min']) , max(self.dados.dados[escolha2+'_min']) )
-				valores +="%s máxima: %.2e - %.2e\n" % (self.dados.dados['more'][self.second.GetSelection()+2],min(self.dados.dados[escolha2+'_max']) , max(self.dados.dados[escolha2+'_max']) )
-
-			
+		valores = output.saida(self)
 		self.results= valores
 		self.text.SetValue(self.results)
+		
 					
 	def quitar(self,event):
 		quit()
