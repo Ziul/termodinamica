318bcf882d7ac5e9fc9e448985d5c30aeadefd81
ziuloliveira@gmail.com <ziuloliveira@gmail.com@ceb656d0-5844-6d09-e0a4-2b68a341c80b>
2013-07-13 21:29:44 +0000

    Provavel finalização do ciclo de rankine

--- trunk/Feipa/rankine.py
+++ trunk/Feipa/rankine.py
@@ -21,16 +21,31 @@ except:
 	print "Erro ao importar a wxPython"
 	print "Consute http://sourceforge.net/projects/wxpython/files/wxPython/2.9.4.0/"
 
+lista=['10', '50', '100' , '200',  '300',  '400',  '500' , '600',  '800','1000', '2000','3000','4000','5000','10000','20000','30000','40000','50000','60000']
+dado=waterNext('./a6/' + lista[0] + '.csv')
 class data(object):
-	valores=[]
-	
 	def calculator(self,event):
-		valores = [	float(self.h1.GetValue()),
-					float(self.h2.GetValue()),
-					float(self.h3.GetValue()),
-					float(self.h4.GetValue())]
-		print valores
-		self.saida.SetLabel("Saída = "+str(valores))
+		valores=[]
+		h=[0,0,0,0]
+		try:			
+			valores = [	float(self.t1.GetValue()),
+						float(self.t2.GetValue()),
+						float(self.t3.GetValue()),
+						float(self.t4.GetValue()),
+						int(self.p1.GetValue()),
+						int(self.p2.GetValue()),
+						int(self.p3.GetValue()),
+						int(self.p4.GetValue())]
+			for i in range(4):
+				dado=waterNext('./a6/' + str(valores[4+i]) + '.csv')
+				inter = sp.interp1d(dado.dados['Temperature'], dado.dados['Enthalpy'],kind='linear')
+				h[i]=float(inter(valores[i]))
+			saida=h[2] - h[3] -h[0] + h[1]
+			print h
+			self.saida.SetLabel("Saída = "+str(saida)+ ' kJ/kg')
+		except Exception, ex:
+			self.saida.SetLabel("Saída = -----")
+			print ex
 	
 def rankine(path):
 	app = wx.App(False)
@@ -43,15 +58,23 @@ def rankine(path):
 	img = img.Scale(img.GetWidth()*0.8,img.GetHeight()*0.8)
 	png = wx.StaticBitmap(panel, -1, wx.BitmapFromImage(img),pos=(0,0))
 	
-	wx.StaticText(panel, label='H1 =', pos=(760, 65))
-	d.h1=wx.TextCtrl(panel, -1, "1", size=(50, -1),pos=(800,60),style=wx.TE_PROCESS_ENTER)
-	wx.StaticText(panel, label='H2 =', pos=(760, 105))
-	d.h2=wx.TextCtrl(panel, -1, "1", size=(50, -1),pos=(800,100),style=wx.TE_PROCESS_ENTER)
-	wx.StaticText(panel, label='H3 =', pos=(760, 145))
-	d.h3=wx.TextCtrl(panel, -1, "1", size=(50, -1),pos=(800,140),style=wx.TE_PROCESS_ENTER)
-	wx.StaticText(panel, label='H4 =', pos=(760, 185))
-	d.h4=wx.TextCtrl(panel, -1, "1", size=(50, -1),pos=(800,180),style=wx.TE_PROCESS_ENTER)
-	calc = wx.Button(panel, 1, "Calcular",(780,220))
+	wx.StaticText(panel, label='T1 =\t\t  P1=', pos=(660, 65))
+	d.t1=wx.TextCtrl(panel, -1, "300", size=(50, -1),pos=(700,60),style=wx.TE_PROCESS_ENTER)
+	d.p1=wx.ComboBox(panel, pos=(800, 60), size=(90, -1), choices=lista, style=wx.CB_SORT)
+	d.p1.SetSelection(0)
+	wx.StaticText(panel, label='T2 =\t\t  P2=', pos=(660, 105))
+	d.t2=wx.TextCtrl(panel, -1, "300", size=(50, -1),pos=(700,100),style=wx.TE_PROCESS_ENTER)
+	d.p2=wx.ComboBox(panel, pos=(800, 100), size=(90, -1), choices=lista, style=wx.CB_SORT)
+	d.p2.SetSelection(0)
+	wx.StaticText(panel, label='T3 =\t\t  P3=', pos=(660, 145))
+	d.t3=wx.TextCtrl(panel, -1, "300", size=(50, -1),pos=(700,140),style=wx.TE_PROCESS_ENTER)
+	d.p3=wx.ComboBox(panel, pos=(800, 140), size=(90, -1), choices=lista, style=wx.CB_SORT)
+	d.p3.SetSelection(0)
+	wx.StaticText(panel, label='T4 =\t\t  P4=', pos=(660, 185))
+	d.t4=wx.TextCtrl(panel, -1, "300", size=(50, -1),pos=(700,180),style=wx.TE_PROCESS_ENTER)
+	d.p4=wx.ComboBox(panel, pos=(800, 180), size=(90, -1), choices=lista, style=wx.CB_SORT)
+	d.p4.SetSelection(0)
+	calc = wx.Button(panel, 1, "Calcular",(680,220))
 	d.saida = wx.StaticText(panel, label='Saída =', pos=(700, 260))
 	panel.Bind(wx.EVT_BUTTON, d.calculator, calc)
 	app.MainLoop()
