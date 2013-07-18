9ca33d426b62996b69f1d63b4e3d2d01a10461c0
ziuloliveira@gmail.com <ziuloliveira@gmail.com@ceb656d0-5844-6d09-e0a4-2b68a341c80b>
2013-07-18 03:01:27 +0000

    POG para progredir

--- trunk/Paulo/main.py
+++ trunk/Paulo/main.py
@@ -2,6 +2,15 @@
 # -*- coding:utf-8 -*-
 import curses																
 from curses import panel													 
+from saida import *
+
+class Data(object):
+	first=[]
+	second=[]
+	third=''
+	results=''
+	def __init__(self):
+		pass
 
 class Menu(object):														  
 
@@ -24,6 +33,14 @@ class Menu(object):
 		elif self.position >= len(self.items):							   
 			self.position = len(self.items)-1								
 
+	def calc(self):
+			data = Data()
+			data.first.append(self.items[0][0])
+			data.second.append(self.items[1][0])
+			data.first.append(self.values[0])
+			data.second.append(self.values[1])
+			self.window.addstr(5,3,saida(data),curses.A_NORMAL)
+			
 	def display(self):													   
 		self.panel.top()													 
 		self.panel.show()													
@@ -31,7 +48,8 @@ class Menu(object):
 
 		while True:														  
 			self.window.refresh()											
-			curses.doupdate()												
+			curses.doupdate()
+			msg=''
 			for index, item in enumerate(self.items):
 				if index == self.position:								   
 					mode = curses.A_REVERSE								  
@@ -40,8 +58,8 @@ class Menu(object):
 				if(len(item)<=1):
 					msg = '%d. %s' % (index, item[0])  + str(self.values[index])
 				else:
-					msg = '%d. < %s >'.ljust(20) % (index, item[0]) +'\t' + str(self.values[index]).rjust(5)
-				self.window.addstr(1+index, 1, msg, mode)					
+					msg = '%d. < %s >'.ljust(20) % (index, item[0]) +'\t' + str(self.values[index]).rjust(5) + '\n'
+				self.window.addstr(1+index, 1, msg, mode)		
 
 			#key = self.window.getch()
 			key = self.window.getkey()
@@ -49,6 +67,8 @@ class Menu(object):
 			if (key == 'KEY_ENTER') or (key == '\n'):
 				if(self.position== 2):
 					break;
+				else:
+					self.calc()
 			
 			if key == 'KEY_UP':									   
 				self.navigate(-1)											
@@ -64,13 +84,16 @@ class Menu(object):
 				self.items[self.position].insert(0,self.items[self.position].pop())
 				
 			else:
-				if(self.position != len(self.items)-1):
-					curses.echo()
-					curses.setsyx(self.position,3)
-					texto = key + self.window.getstr()
-					curses.noecho()
-					self.values[self.position] = float(texto)
-				
+				try:
+
+					if(self.position != len(self.items)-1):
+						self.window.addstr(10, 10, key, curses.A_NORMAL)
+						curses.echo()
+						texto = key + self.window.getstr()
+						self.values[self.position] = float(texto)
+						curses.noecho()
+				except Exception, ex:
+					pass
 			self.window.clear()												  
 			self.panel.hide()													
 			panel.update_panels()												
